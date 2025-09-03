518d2b8ccb3e23b0fbe721bf4a9b8ac1a57a9f00
DU\david.pacheco <DAVID.PACHECO@SII.CL>
2025-09-03 16:08:17 -0400

    Limpia los import sobrantes

--- iva-impuestos-descuentos-drools-cover-cloud-ms/src/main/java/arsii/iva/descuentosdrools/cover/cloud/ms/test/RulesIntegrationWithCoverageTest.java
+++ iva-impuestos-descuentos-drools-cover-cloud-ms/src/main/java/arsii/iva/descuentosdrools/cover/cloud/ms/test/RulesIntegrationWithCoverageTest.java
@@ -4,7 +4,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,10 +16,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import arsii.iva.descuentosdrools.cover.cloud.ms.model.Cliente;
 import arsii.iva.descuentosdrools.cover.cloud.ms.model.Factura;
-
-
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 
 @SpringBootTest
