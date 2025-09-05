c2371924aa16ab3375521a316f437a5340bb5768
DU\david.pacheco <DAVID.PACHECO@SII.CL>
2025-09-05 16:03:27 -0400

    pone comentarios

--- drools-coverage-spring-boot-starter/src/main/java/arsii/iva/drools/coverage/DroolsCoverageSpringBootStarterApplication.java
+++ drools-coverage-spring-boot-starter/src/main/java/arsii/iva/drools/coverage/DroolsCoverageSpringBootStarterApplication.java
@@ -6,6 +6,37 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DroolsCoverageSpringBootStarterApplication {
 
+	/*
+	Flujo del starter
+
+	@EnableDroolsCoverage
+
+	Es una anotación que habilita la autoconfiguración.
+
+	Básicamente importa la clase DroolsCoverageAutoConfiguration.
+
+	DroolsCoverageAutoConfiguration
+
+	Registra dos beans en el contexto de Spring:
+
+	DroolsCoverageListener
+
+	DroolsCoverageReporter
+
+	También asocia el listener a todas las sesiones de Drools (KieSession).
+
+	DroolsCoverageListener
+
+	Se engancha a Drools y captura cada regla que se dispara (afterMatchFired).
+
+	Va guardando los nombres de las reglas ejecutadas en un Set<String>.
+
+	DroolsCoverageReporter
+
+	Tiene el método generateReport().
+
+	Este método toma lo que acumuló el listener y lo transforma en un reporte (ej: JSON, CSV, etc., según tu implementación).
+	*/
 	public static void main(String[] args) {
 		SpringApplication.run(DroolsCoverageSpringBootStarterApplication.class, args);
 	}
